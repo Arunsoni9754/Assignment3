a86bf23d96620d31dcf42504dbb586cf5b71e208
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-08-25 21:11:39 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    printf("Enter the number to be check:-");
+    scanf("%d",&i);
+    if(i%2==0)
+    {
+        printf("The number is even");
+    }
+    else
+    {
+        printf("The number is odd");
+    }
+}
