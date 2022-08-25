802f4e69e5e8e29759cd3ac91c1e2f78f6a19de2
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-08-25 21:35:24 +0530

    Create Q16.c

---
+++ Q16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char j;
+    printf("Enter the number to be check:-");
+    scanf("%c",&j);
+    int i=j;
+    if(i>64&&i<91)
+    {
+        printf("Upper case");
+    }
+    else if(i>=97&&i<123)
+    {
+        printf("Lower case");
+    }
+    else
+    {
+        printf("Any special character");
+    }
+}
