77548cb6b44cbe8471722cc084dac88443314acf
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-08-25 21:10:44 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    printf("Enter the number to check the number to be positive or non positive:- ");
+    scanf("%d",&i);
+    if(i>0)
+    {
+    printf("Positive");
+    }
+    else
+    {
+        printf("Non Positive");
+    }
+
+    return 0;
+}
