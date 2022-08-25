57d4f222ae81ddae4cc7f675cadf08a3e8b3e9ff
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-08-25 21:35:01 +0530

    Create Q15.c

---
+++ Q15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    printf("Enter the number to be check:-");
+    scanf("%d",&i);
+    if(i>0)
+    {
+        printf("The number is positive");
+    }
+    else if(i>0)
+    {
+        printf("The number is negative");
+    }
+    else
+    printf("Zero");
+return 0;
+}
